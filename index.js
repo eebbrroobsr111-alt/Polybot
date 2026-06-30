86d9c997a194728b4412892c60fc047c6830258a
eebbrroobsr111-alt <eebbrroobsr111@gmail.com>
2026-07-01 02:57:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-dh
+require('dotenv').config();
+const { ethers } = require('ethers');
+
+console.log("البوت يعمل الآن يا إبراهيم!");
+
+async function main() {
+    // هنا سنضع لاحقاً كود الاتصال بالشبكة والمنصة
+    console.log("جاري تهيئة الاتصال...");
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});

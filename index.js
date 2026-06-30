59b914ff7b4bbe904942434ba6cb6379008511cd
eebbrroobsr111-alt <eebbrroobsr111@gmail.com>
2026-07-01 02:58:51 +0300

    Remove dotenv and ethers setup from index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-require('dotenv').config();
-const { ethers } = require('ethers');
-
-console.log("البوت يعمل الآن يا إبراهيم!");
-
-async function main() {
-    // هنا سنضع لاحقاً كود الاتصال بالشبكة والمنصة
-    console.log("جاري تهيئة الاتصال...");
-}
-
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
+hjh

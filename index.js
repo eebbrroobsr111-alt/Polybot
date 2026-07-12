821e93c4a057033c032611fb84b2c49d639c0396
eebbrroobsr111-alt <eebbrroobsr111@gmail.com>
2026-07-12 21:03:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-require('dotenv').config();
-
-const TelegramBot = require('node-telegram-bot-api');
-
-// تعريف البوت فوراً في البداية
-const tgBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
-
-console.log("تم تعريف البوت بنجاح!");
-
-// بقية كود البوت سيعمل هنا بشكل طبيعي
+dgg
